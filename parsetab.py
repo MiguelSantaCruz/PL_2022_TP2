2c57551c51efedcf6d04f49cf117ba3dd2514381
miguel <a93194@alunos.uminho.pt>
2022-05-14 02:19:49 +0100

    75% completo

--- parsetab.py
+++ parsetab.py
@@ -6,9 +6,9 @@ _tabversion = '3.10'
 
 _lr_method = 'LALR'
 
-_lr_signature = "CLOSELIST CODE COMMENT ENDCODE EXPREG IGNORE LEX LITERALS OPENLIST PRECEDENT PRODUCAO STARTOFCODE STRING TEXT TOKENS YACCPly : Lex Yacc CodigoLex : LEX Listas ExpregularesListas : Listas ListaListas : Lista : TOKENS '=' STRINGLista : LITERALS '=' STRINGLista : IGNORE '=' STRINGLista : PRECEDENT '=' STRINGExpregulares : Expregulares ExpregularExpregulares : Expregular : EXPREG CODEYacc : YACC Listas ProducoesProducoes : Producoes ProducaoProducoes :  Producao : PRODUCAO CODECodigo : STARTOFCODE ListaComandos ENDCODECodigo : ListaComandos : ListaComandos CODEListaComandos : "
+_lr_signature = "CLOSELIST CODE COMMENT ENDCODE EXPREG IGNORE INDEPENDENTCODE LEX LITERALS OPENLIST PRECEDENT PRODUCAO STARTOFCODE STRING TEXT TOKENS YACCPly : Lex Yacc CodigoLex : LEX Listas ExpregularesListas : Listas ListaListas : Lista : TOKENS '=' STRINGLista : LITERALS '=' STRINGLista : IGNORE '=' STRINGLista : PRECEDENT '=' STRINGExpregulares : Expregulares ExpregularExpregulares : Expregular : EXPREG CODEYacc : YACC Listas ProducoesProducoes : Producoes ProducaoProducoes :  Producao : PRODUCAO CODECodigo : STARTOFCODE ListaComandos ENDCODECodigo : ListaComandos : ListaComandos CODEListaComandos : ListaComandos INDEPENDENTCODEListaComandos : "
     
-_lr_action_items = {'LEX':([0,],[3,]),'$end':([1,4,5,7,9,11,17,24,26,29,30,31,32,33,],[0,-17,-4,-1,-14,-3,-12,-16,-13,-5,-6,-7,-8,-15,]),'YACC':([2,3,6,10,11,18,28,29,30,31,32,],[5,-4,-10,-2,-3,-9,-11,-5,-6,-7,-8,]),'TOKENS':([3,5,6,9,11,29,30,31,32,],[-4,-4,12,12,-3,-5,-6,-7,-8,]),'LITERALS':([3,5,6,9,11,29,30,31,32,],[-4,-4,13,13,-3,-5,-6,-7,-8,]),'IGNORE':([3,5,6,9,11,29,30,31,32,],[-4,-4,14,14,-3,-5,-6,-7,-8,]),'PRECEDENT':([3,5,6,9,11,29,30,31,32,],[-4,-4,15,15,-3,-5,-6,-7,-8,]),'EXPREG':([3,6,10,11,18,28,29,30,31,32,],[-4,-10,19,-3,-9,-11,-5,-6,-7,-8,]),'STARTOFCODE':([4,5,9,11,17,26,29,30,31,32,33,],[8,-4,-14,-3,-12,-13,-5,-6,-7,-8,-15,]),'PRODUCAO':([5,9,11,17,26,29,30,31,32,33,],[-4,-14,-3,27,-13,-5,-6,-7,-8,-15,]),'ENDCODE':([8,16,25,],[-19,24,-18,]),'CODE':([8,16,19,25,27,],[-19,25,28,-18,33,]),'=':([12,13,14,15,],[20,21,22,23,]),'STRING':([20,21,22,23,],[29,30,31,32,]),}
+_lr_action_items = {'LEX':([0,],[3,]),'$end':([1,4,5,7,9,11,17,24,27,30,31,32,33,34,],[0,-17,-4,-1,-14,-3,-12,-16,-13,-5,-6,-7,-8,-15,]),'YACC':([2,3,6,10,11,18,29,30,31,32,33,],[5,-4,-10,-2,-3,-9,-11,-5,-6,-7,-8,]),'TOKENS':([3,5,6,9,11,30,31,32,33,],[-4,-4,12,12,-3,-5,-6,-7,-8,]),'LITERALS':([3,5,6,9,11,30,31,32,33,],[-4,-4,13,13,-3,-5,-6,-7,-8,]),'IGNORE':([3,5,6,9,11,30,31,32,33,],[-4,-4,14,14,-3,-5,-6,-7,-8,]),'PRECEDENT':([3,5,6,9,11,30,31,32,33,],[-4,-4,15,15,-3,-5,-6,-7,-8,]),'EXPREG':([3,6,10,11,18,29,30,31,32,33,],[-4,-10,19,-3,-9,-11,-5,-6,-7,-8,]),'STARTOFCODE':([4,5,9,11,17,27,30,31,32,33,34,],[8,-4,-14,-3,-12,-13,-5,-6,-7,-8,-15,]),'PRODUCAO':([5,9,11,17,27,30,31,32,33,34,],[-4,-14,-3,28,-13,-5,-6,-7,-8,-15,]),'ENDCODE':([8,16,25,26,],[-20,24,-18,-19,]),'CODE':([8,16,19,25,26,28,],[-20,25,29,-18,-19,34,]),'INDEPENDENTCODE':([8,16,25,26,],[-20,26,-18,-19,]),'=':([12,13,14,15,],[20,21,22,23,]),'STRING':([20,21,22,23,],[30,31,32,33,]),}
 
 _lr_action = {}
 for _k, _v in _lr_action_items.items():
@@ -17,7 +17,7 @@ for _k, _v in _lr_action_items.items():
       _lr_action[_x][_k] = _y
 del _lr_action_items
 
-_lr_goto_items = {'Ply':([0,],[1,]),'Lex':([0,],[2,]),'Yacc':([2,],[4,]),'Listas':([3,5,],[6,9,]),'Codigo':([4,],[7,]),'Expregulares':([6,],[10,]),'Lista':([6,9,],[11,11,]),'ListaComandos':([8,],[16,]),'Producoes':([9,],[17,]),'Expregular':([10,],[18,]),'Producao':([17,],[26,]),}
+_lr_goto_items = {'Ply':([0,],[1,]),'Lex':([0,],[2,]),'Yacc':([2,],[4,]),'Listas':([3,5,],[6,9,]),'Codigo':([4,],[7,]),'Expregulares':([6,],[10,]),'Lista':([6,9,],[11,11,]),'ListaComandos':([8,],[16,]),'Producoes':([9,],[17,]),'Expregular':([10,],[18,]),'Producao':([17,],[27,]),}
 
 _lr_goto = {}
 for _k, _v in _lr_goto_items.items():
@@ -27,23 +27,24 @@ for _k, _v in _lr_goto_items.items():
 del _lr_goto_items
 _lr_productions = [
   ("S' -> Ply","S'",1,None,None,None),
-  ('Ply -> Lex Yacc Codigo','Ply',3,'p_Ply','ply-simple.py',99),
-  ('Lex -> LEX Listas Expregulares','Lex',3,'p_Lex','ply-simple.py',102),
-  ('Listas -> Listas Lista','Listas',2,'p_Listas_list','ply-simple.py',105),
-  ('Listas -> <empty>','Listas',0,'p_Listas_empty','ply-simple.py',108),
-  ('Lista -> TOKENS = STRING','Lista',3,'p_Lista_tokens','ply-simple.py',111),
-  ('Lista -> LITERALS = STRING','Lista',3,'p_Lista_literals','ply-simple.py',114),
-  ('Lista -> IGNORE = STRING','Lista',3,'p_Lista_ignore','ply-simple.py',117),
-  ('Lista -> PRECEDENT = STRING','Lista',3,'p_Lista_precedent','ply-simple.py',120),
-  ('Expregulares -> Expregulares Expregular','Expregulares',2,'p_Expregulares_list','ply-simple.py',123),
-  ('Expregulares -> <empty>','Expregulares',0,'p_Expregulares_empty','ply-simple.py',126),
-  ('Expregular -> EXPREG CODE','Expregular',2,'p_Expregular_exp','ply-simple.py',129),
-  ('Yacc -> YACC Listas Producoes','Yacc',3,'p_Yacc_list','ply-simple.py',132),
-  ('Producoes -> Producoes Producao','Producoes',2,'p_Producoes_notempty','ply-simple.py',135),
-  ('Producoes -> <empty>','Producoes',0,'p_Producoes_empty','ply-simple.py',138),
-  ('Producao -> PRODUCAO CODE','Producao',2,'p_Producao_','ply-simple.py',141),
-  ('Codigo -> STARTOFCODE ListaComandos ENDCODE','Codigo',3,'p_Codigo_notempty','ply-simple.py',144),
-  ('Codigo -> <empty>','Codigo',0,'p_Codigo_empty','ply-simple.py',147),
-  ('ListaComandos -> ListaComandos CODE','ListaComandos',2,'p_ListaComandos_notempty','ply-simple.py',150),
-  ('ListaComandos -> <empty>','ListaComandos',0,'p_ListaComandos_empty','ply-simple.py',153),
+  ('Ply -> Lex Yacc Codigo','Ply',3,'p_Ply','ply-simple.py',106),
+  ('Lex -> LEX Listas Expregulares','Lex',3,'p_Lex','ply-simple.py',109),
+  ('Listas -> Listas Lista','Listas',2,'p_Listas_list','ply-simple.py',113),
+  ('Listas -> <empty>','Listas',0,'p_Listas_empty','ply-simple.py',116),
+  ('Lista -> TOKENS = STRING','Lista',3,'p_Lista_tokens','ply-simple.py',119),
+  ('Lista -> LITERALS = STRING','Lista',3,'p_Lista_literals','ply-simple.py',123),
+  ('Lista -> IGNORE = STRING','Lista',3,'p_Lista_ignore','ply-simple.py',128),
+  ('Lista -> PRECEDENT = STRING','Lista',3,'p_Lista_precedent','ply-simple.py',132),
+  ('Expregulares -> Expregulares Expregular','Expregulares',2,'p_Expregulares_list','ply-simple.py',136),
+  ('Expregulares -> <empty>','Expregulares',0,'p_Expregulares_empty','ply-simple.py',139),
+  ('Expregular -> EXPREG CODE','Expregular',2,'p_Expregular_exp','ply-simple.py',142),
+  ('Yacc -> YACC Listas Producoes','Yacc',3,'p_Yacc_list','ply-simple.py',165),
+  ('Producoes -> Producoes Producao','Producoes',2,'p_Producoes_notempty','ply-simple.py',169),
+  ('Producoes -> <empty>','Producoes',0,'p_Producoes_empty','ply-simple.py',172),
+  ('Producao -> PRODUCAO CODE','Producao',2,'p_Producao','ply-simple.py',175),
+  ('Codigo -> STARTOFCODE ListaComandos ENDCODE','Codigo',3,'p_Codigo_notempty','ply-simple.py',182),
+  ('Codigo -> <empty>','Codigo',0,'p_Codigo_empty','ply-simple.py',185),
+  ('ListaComandos -> ListaComandos CODE','ListaComandos',2,'p_ListaComandos_code','ply-simple.py',188),
+  ('ListaComandos -> ListaComandos INDEPENDENTCODE','ListaComandos',2,'p_ListaComandos_independentcode','ply-simple.py',193),
+  ('ListaComandos -> <empty>','ListaComandos',0,'p_ListaComandos_empty','ply-simple.py',197),
 ]
--- ply-simple.py
+++ ply-simple.py
@@ -1,9 +1,12 @@
 import ply.lex as lex 
 import ply.yacc as yacc
+import re
+
+pcount = 0
 
 literals = ['=',',']
 
-tokens = ['LEX','STRING','OPENLIST','CLOSELIST','LITERALS','IGNORE','TOKENS','EXPREG','CODE','YACC','PRECEDENT','COMMENT','PRODUCAO','STARTOFCODE','TEXT','ENDCODE']
+tokens = ['LEX','STRING','OPENLIST','CLOSELIST','LITERALS','IGNORE','TOKENS','EXPREG','CODE','YACC','PRECEDENT','COMMENT','PRODUCAO','STARTOFCODE','TEXT','ENDCODE','INDEPENDENTCODE']
 
 states = [('initial','inclusive'),
           ('list','exclusive'),
@@ -47,7 +50,7 @@ def t_comment_END(t):
     lexer.pop_state()
 
 def t_EXPREG(t):
-    r'r\'.*\''
+    r'r\'[^\']*\''
     lexer.push_state('expreg')
     return t
 
@@ -78,14 +81,19 @@ def t_function_STRING(t):
     lexer.pop_state()
     return t
 
-def t_code_CODE(t):
+def t_code_INDEPENDENTCODE(t):
     r'(.)+\n'
+    return t
+
+def t_code_CODE(t):
+    r'def(.)*return.*\n'
     return t
 
 def t_code_ENDCODE(t):
     r'/\$\$'
     lexer.pop_state()
     return t
+
 
 t_ANY_ignore = " \t\n\r"
 
@@ -100,6 +108,7 @@ def p_Ply(p):
 
 def p_Lex(p):
     "Lex : LEX Listas Expregulares"
+    print("import ply.lex as lex")
 
 def p_Listas_list(p):
     "Listas : Listas Lista"
@@ -109,15 +118,20 @@ def p_Listas_empty(p):
 
 def p_Lista_tokens(p):
     "Lista : TOKENS '=' STRING"
+    print("tokens = [" + p[3] + "]")
 
 def p_Lista_literals(p):
     "Lista : LITERALS '=' STRING"
+    print("literals = [" + p[3] + "]")
+    
 
 def p_Lista_ignore(p):
     "Lista : IGNORE '=' STRING"
+    print("ignore = [" + p[3] + "]")
 
 def p_Lista_precedent(p):
     "Lista : PRECEDENT '=' STRING"
+    print("precedent = [" + p[3] + "]")
 
 def p_Expregulares_list(p):
     "Expregulares : Expregulares Expregular"
@@ -127,9 +141,30 @@ def p_Expregulares_empty(p):
 
 def p_Expregular_exp(p):
     "Expregular : EXPREG CODE"
+    string = re.split(r"\(",p[2])
+    if string[0] == "return":
+        returnArgs = re.split(r"\'",p[2])
+        print("def t_" + returnArgs[1] + "(t):\n\t",end="")
+        print(p[1])
+        code = returnArgs[2].split(",")
+        print("\tt.value =" + code[1][:-1])
+        print("\treturn t")
+    elif string[0] == "error":
+        print("def t_error(t):")
+        # Separar error(blabla, bla) em "error"" e "blabla, bla)"
+        errorCode = p[2].split("(",1)
+        # Transformar "blabla, bla)" em ")alb, albalb"
+        errorCodeReversed = errorCode[1][::-1]
+        # Fazer split pela virgula
+        parsedString = str(errorCodeReversed).split(",",1)
+        print("\tprint(" + parsedString[1][::-1] + ")")
+        parsedString = parsedString[0][::-1]
+        print("\t" + parsedString[:-1])
+    print("")
 
 def p_Yacc_list(p):
     "Yacc : YACC Listas Producoes"
+    print("import ply.yacc as yacc")
 
 def p_Producoes_notempty(p):
     "Producoes : Producoes Producao"
@@ -137,8 +172,12 @@ def p_Producoes_notempty(p):
 def p_Producoes_empty(p):
     "Producoes :  "
 
-def p_Producao_(p):
+def p_Producao(p):
     "Producao : PRODUCAO CODE"
+    global pcount
+    print("def p_" + str(pcount) + "(p):\n\t\"" + p[1].rstrip(" ") + "\"\n\t" + p[2][1:])
+    pcount = pcount + 1
+    print("")
 
 def p_Codigo_notempty(p):
     "Codigo : STARTOFCODE ListaComandos ENDCODE"
@@ -146,16 +185,21 @@ def p_Codigo_notempty(p):
 def p_Codigo_empty(p):
     "Codigo : "
 
-def p_ListaComandos_notempty(p):
+def p_ListaComandos_code(p):
     "ListaComandos : ListaComandos CODE"
-
+    words = p[2].split(" ")
+    print(p[2],end="")
+    
+def p_ListaComandos_independentcode(p):
+    "ListaComandos : ListaComandos INDEPENDENTCODE"
+    print()
+    
 def p_ListaComandos_empty(p):
     "ListaComandos : "
 
 def p_error(p):
     print("Syntax Error: ",p)
     parser.success = False
-
 
 parser = yacc.yacc()
 
@@ -166,9 +210,12 @@ parser.success = True
 
 
 program = sys.stdin.read()
-parser.parse(program)
-if parser.success:
-    print("Programa estruturalmente correto!")
-else:
-    print("Programa com erros... Corrija e tente novamente!")
+#parser.parse(program)
+#if parser.success:
+#    print("Programa estruturalmente correto!")
+#else:
+#    print("Programa com erros... Corrija e tente novamente!")
 
+lexer.input(program)
+for tok in lexer:
+    print(tok)
